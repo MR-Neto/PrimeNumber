ce9b4fb8c38a0ef5058e5d8e89125a4371c63eea
Miguel Ribeiro Neto <miguelribeironeto@MBP-de-Miguel.home>
2019-04-11 18:50:09 +0200

    Added Mocha

--- prime.js
+++ prime.js
@@ -8,7 +8,35 @@ const cleanPrimes = array.reduce((acc, cur) => {
   return acc.concat(cur.trimRight().substr(cur.indexOf(' ') + 1).split(' '));
 }, [])
 
-console.log(cleanPrimes);
+
+const firstPrimeNumber = (inputNumber) => {
+
+  if (!inputNumber || inputNumber.toString().length!==3) {
+    return "WRONG INPUT";
+  }
+
+  for (let checkNumber of cleanPrimes) {
+    const sizeCheck = checkNumber.length >= inputNumber.length;
+
+    const firstNumberIndex = checkNumber.indexOf(inputNumber.toString().charAt(0));
+    const secondNumberIndex = checkNumber.substr(firstNumberIndex+1).indexOf(inputNumber.toString().charAt(1))+firstNumberIndex+1;
+    const thirdNumberIndex = checkNumber.substr(secondNumberIndex+1).indexOf(inputNumber.toString().charAt(2))+secondNumberIndex+1;
+
+    if (sizeCheck && firstNumberIndex != -1 && firstNumberIndex < secondNumberIndex && secondNumberIndex != -1 && secondNumberIndex < thirdNumberIndex) {
+      return checkNumber;
+    }
+  }
+  return false;
+}
+
+
+// console.log(firstPrimeNumber());
+// console.log(firstPrimeNumber(2));
+// console.log(firstPrimeNumber(232));
+// console.log(firstPrimeNumber(''));
+// console.log(firstPrimeNumber('23'));
+// console.log(firstPrimeNumber('kjl'));
 
 
 
+module.exports=firstPrimeNumber;
---
+++ test/test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const firstPrimeNumber = require('../prime');
+
+describe('First Prime Number', function () {
+  it('should return -1 when the value is not present', function () {
+    assert.equal(firstPrimeNumber("277"), "277");
+  });
+  it('should return -1 when the value is not present', function () {
+    assert.equal(firstPrimeNumber("277"), "277");
+  });
+  it('should return -1 when the value is not present', function () {
+    assert.equal(firstPrimeNumber("277"), "277");
+  });
+  it('should return -1 when the value is not present', function () {
+    assert.equal(firstPrimeNumber("277"), "277");
+  });
+});
